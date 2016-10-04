b266094a12192b101e090be54362aafab2937c1b
volaya <volayaf@gmail.com>
2016-10-04 12:33:32 +0200

    added prestep to transformcsv lesson

--- general/transformcsv/__init__.py
+++ general/transformcsv/__init__.py
@@ -6,6 +6,12 @@ from lessons import addLessonModule
 lesson = Lesson ("Transform X and Y CSV table into a point vector layer",
                  "General tasks", "lesson.html")
 
+def copyTable():
+	filepath = os.path.join(os.path.dirname(os.path.dirname(__file__)), "_data", "songs.csv")
+	copyLessonData(filepath, os.path.basename(os.path.dirname(__file__)))
+
+lesson.addStep("Copy data", "Copy data", copyTable)
+
 lesson.addMenuClickStep('Layer/Add Layer/Add Delimited Text Layer...')
 
 lesson.addStep('Configure importing settings', 'delimited_test_settings.html')
