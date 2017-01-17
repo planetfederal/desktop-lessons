bd08f56bb56a81a1e91c4e4751008e7b7bb2b5e7
volaya <volayaf@gmail.com>
2017-01-17 15:18:22 +0100

    improved build script and added manifest files

--- build.py
+++ build.py
@@ -1,21 +1,15 @@
 import os
 import zipfile
+import shutil
 
 def main ():
     exclude = [".git"]
     folder = os.path.dirname(os.path.realpath(__file__))
-    print folder
     for name in os.listdir(folder):
         if name not in exclude:
             fullpath = os.path.join(folder, name)
             if os.path.isdir(fullpath):          
-                zipf = zipfile.ZipFile(name + '.zip', 'w', zipfile.ZIP_DEFLATED)
-                for root, dirs, files in os.walk(fullpath):
-                    for f in files:
-                        zipf.write(os.path.join(root, f), os.path.join(name, f))
-                zipf.close()                    
-
-
+                shutil.make_archive(os.path.join(folder, name), "zip", fullpath)
 
 if __name__ == '__main__':
     main()
