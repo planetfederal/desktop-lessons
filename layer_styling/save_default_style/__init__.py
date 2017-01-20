07eb34128e827f52f0fd66132336c5405bb1b1a1
Alexandre Neto <senhor.neto@gmail.com>
2017-01-20 10:47:07 +0000

    Adds save default style

---
+++ layer_styling/save_default_style/__init__.py
@@ -0,0 +1,40 @@
+from lessons.lesson import Lesson, Step
+from lessons.utils import *
+from qgis.utils import iface
+from lessons import addLessonModule
+
+# Lesson's description
+
+lesson = Lesson ("Setting layer's default style", "Layer styling", "00_lesson.html")
+
+
+# Copy dataset to the user's folder
+def copyTable():
+    filepath = os.path.join(os.path.dirname(os.path.dirname(__file__)), "save_default_style/data", "Wake_BlockGroup_2010.zip")
+    copyLessonData(filepath, os.path.basename(os.path.dirname(__file__)))
+
+lesson.addStep("Copy data", "Copy data", copyTable)
+
+
+# Steps
+
+lesson.addStep('Introduction', '01_intro.html', steptype=Step.MANUALSTEP)
+
+lesson.addMenuClickStep('Layer/Add Layer/Add Vector Layer...')
+
+lesson.addStep('Load example dataset', '02_select_dataset_file.html', steptype=Step.MANUALSTEP)
+
+lesson.addMenuClickStep('Layer/Properties...', '03_open_properties.html')
+
+lesson.addStep('Change layer style', '04_change_settings.html', steptype=Step.MANUALSTEP)
+
+lesson.addMenuClickStep('Layer/Add Layer/Add Vector Layer...', '05_open_dataset_again.html')
+
+lesson.addStep('Load example dataset', '02_select_dataset_file.html', steptype=Step.MANUALSTEP)
+
+lesson.addStep('Conclusion', '06_conclusion.html', steptype=Step.MANUALSTEP)
+
+
+# Suggested lessons
+
+lesson.addNextLesson("Layer styling", "Manage Multiple Styles")
