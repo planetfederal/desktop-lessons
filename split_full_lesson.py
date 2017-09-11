0d7fa69d4a3cd68d3dd07d3bbd8e1450263462a4
Alexandre Neto <senhor.neto@gmail.com>
2017-09-11 12:10:47 +0100

    update script to create yaml lessons with new structure

--- split_full_lesson.py
+++ split_full_lesson.py
@@ -6,15 +6,17 @@ import yaml
 def main ():
 
     # Get file path from first argument passed in command line
-    fl_path = sys.argv[2]
+    fl_path = sys.argv[1]
     folder = os.path.dirname(fl_path)
     s_file = None
     s_list= []
-    l_structure = {'name': '',
+    l_structure = {'lesson': { 'en': {
+                   'name': '',
                    'group': '',
                    'description': 'lesson.md',
                    'steps': [],
-                   'nextLessons': [{'name': '', 'group': ''}]}
+                   'nextLessons': [{'name': '', 'group': ''}]}}}
+    l_structure_en = l_structure['lesson']['en']
     s_number = 0
     with open(fl_path) as full_lesson:
         for line in full_lesson:
@@ -26,7 +28,7 @@ def main ():
 
             if l:
                 l_title = (l.group(0))[2:]
-                l_structure['name'] = l_title
+                l_structure_en['name'] = l_title
                 s_file = open(os.path.join(folder, 'lesson.md'), 'w+')
                 next(full_lesson) # skip empty line after heading
 
@@ -36,7 +38,7 @@ def main ():
                 s_title = (s.group(0))[3:]
                 s_file_name = '{0:02d}_{1}.md'.format(s_number, s_title.replace(
                     ' ', '_').lower())
-                l_structure['steps'].append({'name':l_title,
+                l_structure_en['steps'].append({'name':s_title,
                                              'description': s_file_name})
                 s_list.append((s_title, s_file_name))
 
