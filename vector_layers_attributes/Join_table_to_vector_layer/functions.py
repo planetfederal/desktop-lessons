ed8cb6334dd3f28f583e930b068859a02bdefb46
Alexandre Neto <senhor.neto@gmail.com>
2017-11-13 15:00:20 +0000

    Adds better endCheck function to  check if join exists

--- vector_layers_attributes/Join_table_to_vector_layer/functions.py
+++ vector_layers_attributes/Join_table_to_vector_layer/functions.py
@@ -4,22 +4,33 @@ from lessons.utils import layerFromName
 
 # EndCheck Function
 
-def fieldExists(layername, string):
+def checkJoinExists (layername, fieldname, targetlayer, targetfield):
     """
-    Check if a layer has a field which name contains the specified string
-    The layer should be of the vector type and be loaded into project.
-    :param layername: Layer's name as visible in the layers panel
-    :param string: string of text
-
+    Returns True if vector layer has a join with a
+    target layer using the specified field and target field
+    :param layername: vector layer name
+    :param fieldname: join field name
+    :param targetlayer: target vector layer name
+    :param targetfield: target join field name
     :return: Boolean
     """
 
     layer = layerFromName(layername)
-    if layer is None:
+    tlayer = layerFromName(targetlayer)
+
+    # check if layers exist
+    if layer is None or tlayer is None:
         return False
 
-    for field in layer.pendingFields():
-        if string in field.name():
+    # check if join fields exist
+    if fieldname not in [f.name() for f in layer.pendingFields()] or \
+       targetfield not in [f.name() for f in tlayer.pendingFields()]:
+        return False
+
+    join_settings = (tlayer.id(), unicode(targetfield), unicode(fieldname))
+
+    for j in layer.vectorJoins():
+        if (j.joinLayerId, j.joinFieldName, j.targetFieldName) == join_settings:
             return True
 
     return False
